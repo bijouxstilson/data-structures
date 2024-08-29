fd8b598e1db9ed3231097bc7b9474274c1c198e7
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-08-29 13:59:22 -0500

    pair programming linked lists

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -1,10 +1,12 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Business utility methods.
 */
 public class Business
 {
+    
     /**
       * Removes every nth element from the linked list
       *
@@ -13,6 +15,20 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        ...
+       ListIterator<String> iterator = employeeNames.listIterator();
+        int count = 0;
+
+
+       while (iterator.hasNext()){
+          count+=1;
+          iterator.next();
+          if (count % n == 0){
+            iterator.remove();
+          }
+
+       } 
+        System.out.println(employeeNames);
+       
     }
-}
+    }
+

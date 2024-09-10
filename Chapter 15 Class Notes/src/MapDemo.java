8d3c0cb2a75d4c5cf353f9e7ee46e457bae95d69
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-09-10 14:20:22 -0500

    Map Demo Work

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,5 +10,37 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+
+        /*
+         * The Map interface is generic.
+         * The first type given is the type of the key.
+         * The second type given is the type of the value.
+         * 
+         */
+        // Map<String, Color> favColors = new HashMap<>();
+
+        Map<String/*key is a string */, Color /*value is a color */> favColors = new HashMap<>();
+
+        //add elements with .put();
+        favColors.put("Henry", Color.BLUE);
+        favColors.put("Jack", Color.RED);
+        favColors.put("Priya", Color.GREEN);
+
+        //two different elements with the same value is possible
+        favColors.put("Dr.Miller", Color.GREEN); 
+        
+        //same key CANNOT have two different values
+        //using put on a key that exists changes the value
+        favColors.put("Jack", Color.MAGENTA);
+
+        //create a set of the keys in a map
+        Set<String> keys = favColors.keySet();
+
+        for (String key: keys) {
+            System.out.println(key + " (" + key.hashCode() + "): " + favColors.get(key)); //prints everything in the map: key, key's hash code, value you get from key
+
+        }
+
+
     }
 }

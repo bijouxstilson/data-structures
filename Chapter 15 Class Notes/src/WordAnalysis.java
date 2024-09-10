8daa9f613e5eed86d828edf616d52ecf0efcb91a
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-09-10 13:43:14 -0500

    Word Analysis work

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -1,18 +1,29 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * This program checks which words in a file are not present in a dictionary.
 */
+
 public class WordAnalysis
 {
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Read the dictionary file and the novel file
+        Set<String> dictionaryWords = readWords(/* "Software Engineering/data-structures/Projects/*/"Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords(/* "Software Engineering/data-structures/Projects/*/"Chapter 15 Class Notes/src/war-and-peace.txt");
+
+        //print all the words that are in the novel but not in the dictionary
+        for (String word: novelWords){
+            if (!dictionaryWords.contains(novelWords)){
+                System.out.println(word);
+            }
+     } 
+     System.out.println("There are " + novelWords.size() + " unique words in the novel.");
     }
 
     /**
@@ -25,6 +36,19 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        // we use a HashSet instead of a TreeSet because the order doesn't matter
+        Set<String> words = new HashSet<>();
+
+        System.out.println("!!!" + System.getProperty("user.dir"));
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        //use any character that's not a letter as a delimeter
+        in.useDelimiter("[^a-zA-Z]+"); //wth
+        
+        while (in.hasNext()){
+            words.add(in.next().toLowerCase()); 
+            //add words to the set (duplicates are automatically ignored)
+        }
+        return words;
     }
 }

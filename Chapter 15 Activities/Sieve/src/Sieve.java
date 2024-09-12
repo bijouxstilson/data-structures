1e07b0cd592ac943fe9606bccfd977cb3aeef932
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-09-12 09:40:21 -0500

    Sieve practice program work

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 
 /**
@@ -8,12 +10,25 @@ public class Sieve
 {
     public static void main(String[] args)
     {
+        Set<Integer> nums = new HashSet<>();
         Scanner in = new Scanner(System.in);
+
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
 
-        // Your work goes here
-        . . .
+        for (int i = 2; i < n; i++){
+            nums.add(i);
+        }
+
+        for (int j = 2; j < n; j++){
+            for (int k = 2; j*k < n; k++){
+                nums.remove(k*j);
+            }
+        }
+
+        for (int num: nums){
+            System.out.print(num + " ");
+        }
 
 
 

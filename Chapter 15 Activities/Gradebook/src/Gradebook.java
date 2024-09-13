a6003d24a694ec9b9000f38e694c7d0399b7d068
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-09-13 13:38:39 -0500

    Gradebook completion work

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
-. . .
+
+import java.util.*;
+//. . .
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,7 +11,9 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> students = new HashMap<>();
+       Set<String> keys;
+
 
         boolean done = false;
         while(!done)
@@ -22,18 +25,45 @@ public class Gradebook
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("Who would you like to add? ");
+                String name = in.next().toUpperCase();
+             
+                System.out.println("What is their grade? ");
+                
+                String grade = in.next().toUpperCase();
+
+                students.put(name,grade);
+
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Who would you like to remove? ");
+                String name = in.next().toUpperCase();
+                students.remove(name);
+
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("Who would you like to modify? ");
+                String name = in.next().toUpperCase();
+
+                System.out.println("What do you want to change the grade to? ");
+                
+                String grade = in.next().toUpperCase();
+                students.put(name, grade);
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
-            } else
+                ArrayList<String> keyList = new ArrayList<>();
+                keys = students.keySet();
+                for(String key: keys){
+                    keyList.add(key);
+                }
+                Collections.sort(keyList);
+                for(String key: keyList){
+                    System.out.println(key+": "+students.get(key));
+                }
+
+                }
+             else
             {
                 done = true;
             }

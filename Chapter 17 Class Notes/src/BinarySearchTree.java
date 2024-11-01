4a403163da982af07b7ff6e740d48c7905bab48b
bijouxstilson <142510937+bijouxstilson@users.noreply.github.com>
2024-11-01 14:14:21 -0500

    BinarySearchTree in class lesson

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -1,3 +1,7 @@
+
+import java.lang.classfile.components.ClassPrinter;
+import java.util.logging.ErrorManager;
+
 /**
     This class implements a binary search tree whose
     nodes hold objects that implement the Comparable
@@ -12,6 +16,7 @@ public class BinarySearchTree
     */
     public BinarySearchTree()
     {   
+        this.root = null;
         
     }
     
@@ -21,7 +26,19 @@ public class BinarySearchTree
     */
     public void add(Comparable obj) 
     {   
-        
+        Node newNode = new Node();
+        newNode.data=obj;
+        newNode.left = null; newNode.right = null;
+
+        if (this.root == null){
+            root = newNode;
+        }
+        else{
+            this.root.addNode(newNode);
+        }
+
+
+
     }
 
     /**
@@ -31,6 +48,20 @@ public class BinarySearchTree
     */
     public boolean find(Comparable obj)
     {
+        Node current = this.root;
+        while (current != null){
+            int difference = obj.compareTo(current.data);
+            if (difference == 0){
+                return true;
+            }
+            else if (difference < 0){
+                current = current.left;
+            }
+            else{
+                current = current.right;
+            }
+        }
+
         return false;
     }
     
@@ -41,7 +72,46 @@ public class BinarySearchTree
     */
     public void remove(Comparable obj)
     {
-        
+        Node toBeRemoved = this.root;
+        Node parent = null;
+        boolean found = false;
+
+            while(!found && toBeRemoved!= null){
+                int difference = obj.compareTo(toBeRemoved.data);
+                if (difference == 0) { found = true; }
+                else{
+                    parent = toBeRemoved;
+                    if (difference < 0){
+                        toBeRemoved = toBeRemoved.left;
+                    }
+                    else{
+                        toBeRemoved = toBeRemoved.right;
+                    }
+                }
+            }
+
+            if (!found){
+                return;
+            }
+
+            //case1 and case2 (at least one (1) child is null)
+            if (toBeRemoved.left == null || toBeRemoved.right== null){
+                Node newChild;//= new Node();
+
+                if (toBeRemoved.left == null){
+                    newChild = toBeRemoved.right;
+                }
+                else{
+                    newChild = toBeRemoved.left;
+                }
+                //removng the root
+                if (parent == null){
+                    this.root = newChild;
+                }
+                else if (parent.left){
+                    
+                }
+            }
     }
     
     /**
@@ -67,7 +137,9 @@ public class BinarySearchTree
     */
     static class Node
     {   
-        
+    // a bst /MUST/ be made up of Comparable Objects 
+        public Comparable data;
+        public Node left, right;
 
         /**
             Inserts a new node as a descendant of this node.
@@ -75,7 +147,27 @@ public class BinarySearchTree
         */
         public void addNode(Node newNode)
         {   
-            
+            //if difference < 0, newNode is left of this node
+            //if difference > 0, newNode is right of this node
+            int difference = newNode.data.compareTo(data);
+
+            if (difference < 0){ 
+                if (left==null){
+                    left = newNode;
+                }
+                else {
+                    left.addNode(newNode); //recursion! 'tis a joyous day
+                }
+            } //'else if' rather than 'else' insures that we don't accidentally add a duplicate Node
+            else if (difference > 0){ 
+                if (right==null){
+                    right = newNode;
+                }
+                else {
+                    right.addNode(newNode); //recursion! 'tis a joyous day
+                }
+            }
+
         }
     }
 }
